078894ed93e64e017a4c7b43b30b37be3b568524
SanoHiroshi <hroshi.sano0711@gmail.com>
2019-03-03 17:12:49 +0900

    順列による最小範囲から最高範囲の組み合わせ出力

---
+++ perm.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LEN 11
+
+int n, r[LEN];
+void permut(int);
+void action();
+
+int main(int argc, char const *argv[])
+{
+  int i;
+  printf("Enter n (< %d)", LEN); scanf("%d", &n);
+  if(n >= LEN){
+    exit(1);
+  }
+  for(i = 1; i <= n; i++){
+    r[i] = i;
+  }
+  printf("\nOutput: \n\n");
+  permut(1);
+  return 0;
+}
+
+void permut(int k){
+  int i, j, aux;
+
+  if(k == n){
+    action(); return;
+  }
+
+  for(i = k; i <= n; i++){
+    aux = r[i];
+
+    for(j = i; j > k; j--){
+      r[j] = r[j - 1];
+    }
+    r[k] = aux;
+
+    permut(k + 1);
+
+    for(j = k; j < i; j++){
+      r[j] = r[j + 1];
+    }
+    r[i] = aux;
+  }
+}
+
+void action(){
+  int i;
+  for(i = 1; i <= n; i++){
+    printf("%3d", r[i]);
+  }
+  printf("\n");
+}
