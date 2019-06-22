2498249aef9ebac6ac3b27d98ab7545ab4f0eabe
SanoHiroshi <hroshi.sano0711@gmail.com>
2019-06-22 22:54:18 +0900

    合計税込金額、購入ボタンの制御

--- vue-js/js/app.js
+++ vue-js/js/app.js
@@ -1,36 +1,46 @@
-var items = [
+let items = [
     {
         name: '鉛筆',
-        price: 300,
-        quantity: 0,
+        price: 100,
+        quantity: 1,
     },
     {
         name: 'ノート',
-        price: 400,
-        quantity: 0,
+        price: 200,
+        quantity: 2,
     },
     {
         name: '消しゴム',
         price: 500,
-        quantity: 0,
+        quantity: 1,
     },
 ]
-var vm = new Vue({
+
+let vm = new Vue({
     el: '#app',
     data: {
-        items: items
-    }
-})
-vm.$watch(function() {
-  return this.items[0].quantity
-}, function(quantity){
-    console.log(quantity)
-})
-
-var vm = new Vue({
-    el: '#b-button',
-    data: {
+        items: items,
         loggedInButton: 'ログイン済みのため購入できます',
-        canBuy: false
-    }
+        min_purchase_price: 1000,
+        tax_rate: 1.08
+    },
+    filters: {
+        withDelimiter: function(value){
+            if(!value){ return '0' }
+            return value.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+        }
+    },
+    computed: {
+        totalPrice: function(){
+            return this.items.reduce(function (sum, item) {
+                return sum + (item.price * item.quantity)
+            }, 0)
+        },
+        totalPriceWithTax: function(){
+            return Math.floor(this.totalPrice * this.tax_rate)
+        },
+        canBuy: function(){
+            return this.totalPrice >= this.min_purchase_price
+        }
+    },
 })
