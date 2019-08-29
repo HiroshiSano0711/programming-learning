391e5d147b474f1f90dd09529ca84f494754b20c
SanoHiroshi <hroshi.sano0711@gmail.com>
2019-08-29 16:10:12 +0900

    スタックの確認

--- c/stack-test.c
+++ c/stack-test.c
@@ -1,11 +1,16 @@
-int main()
-{
+int add(a, b){
+    int c = a;
+    int d = b;
+    return c + d + 10;
+}
+
+int main(){
     int a;
     int b;
 
     a = 3;
     b = 10;
 
-    a = a + b;
+    add(a,b);
     return 0;
 }
