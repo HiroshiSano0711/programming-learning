e119d0d54e8d4b76a36f7c544c4c6f3aa6230b5f
SanoHiroshi <hroshi.sano0711@gmail.com>
2019-09-01 19:38:58 +0900

    再帰と繰り返しの違いを確認

---
+++ c/assign.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+  user_input = argv[1];
+  printf("%s", user_input);
+  return 0;
+}
--- c/euclid.c
+++ c/euclid.c
@@ -6,53 +6,28 @@ int gcd(int x, int y);
 
 int main(){
   int n;
-
   n = gcd(2400, 400);
-
   printf("%d\n", n);
-
   return 0;
 }
 
-// 擬似コード（効率の悪いコード）＝＞　効率の良いコード
-// 5箇所？
-// int gcd(int x, int y){
-//   int temp, r;
-//   if (x < 0) {
-//     x = -x;
-//   }
-//   if (y < 0) {
-//     y = -y;
-//   }
-//   if (x <= y) {
-//     temp = x; x = y; y = temp;
-//   }
-//   if (y == 0) {
-//     return x;
-//   }
-
-//   while(1){
-//     r = x % y;
-//     if (r == 0) {
-//       return y;
-//     }
-//     x = y;
-//     y = r;
-//   }
-// }
-
-// int gcd(int x, int y){
-//   int r;
-
-//   while(y){
-//     r = x % y;
-//     x = y;
-//     y = r;
-//   }
-//   return x;
-// }
+int gcd(int x, int y){
+  int r;
+  if (x < 0) {
+    x = -x;
+  }
+  if (y < 0) {
+    y = -y;
+  }
+  while(y){
+    r = x % y;
+    x = y;
+    y = r;
+  }
+  return x;
+}
 
 // 再帰版
 int gcd(int x, int y){
-  return y ? gcd(y, x % y) : x;
+  return y ? gcd(y, x % y) : x; // 最適化なし
 }
