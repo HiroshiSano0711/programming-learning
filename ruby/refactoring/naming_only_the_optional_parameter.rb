097605709510f23e90e4bc0aca258dac5bfdff2b
SanoHiroshi <hroshi.sano0711@gmail.com>
2019-10-13 14:41:00 +0900

    naming_only_the_optional_parameterまで

---
+++ ruby/refactoring/naming_only_the_optional_parameter.rb
@@ -0,0 +1,36 @@
+# before
+def self.find(selector, conditions="", *joins)
+  sql = ["SELECT * from books"]
+  joins.each do |join_table|
+    sql << "LEFT OUTER JOIN #{join_table} ON"
+    sql << "books.#{join_table.to_s.chap}_id"
+    sql << " = #{join_table}.id"
+  end
+  sql << "WHERE #{conditions}" unless conditions.empty?
+  sql << "LIMIT 1" if selector == :first
+  connection.find(sql.join(" "))
+end
+
+# after
+def self.find(selector, hash={})
+  hash[:joins] ||= []
+  hash[:conditions] ||= ""
+  sql = ["SELECT * from books"]
+  hash[:joins].each do |join_table|
+    sql << "LEFT OUTER JOIN #{join_table} ON"
+    sql << "books.#{join_table.to_s.chap}_id"
+    sql << " = #{join_table}.id"
+  end
+  sql << "WHERE #{hash[:conditions]}" unless hash[:conditions].empty?
+  sql << "LIMIT 1" if selector == :first
+  connection.find(sql.join(" "))
+end
+
+module AssertValidKeys
+  def aseert_valid_keys(*valid_keys)
+    unknown_keys = key - [valid_keys].flatten
+    if unknown_keys.any?
+      raise(ArgumentError, "Unknown Key(s): #{unknown_keys.join(', ')}")
+    end
+  end
+end
