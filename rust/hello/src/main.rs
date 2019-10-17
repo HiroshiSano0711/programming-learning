89fc1c1b00e2b54f6c303b37932cc5d1cf8fa668
SanoHiroshi <hroshi.sano0711@gmail.com>
2019-10-17 18:03:31 +0900

    標準入力から値を受け取って最大公約数を求める

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -1,6 +1,23 @@
+// トレイトをスコープに取り込む。型が実装できるメソッドの集合。
+use std::io::Write;
+use std::str::FromStr;
+
 fn main() {
-    let a = gcd(1000, 100);
-    println!("{}", a);
+    let mut numbers = Vec::new(); // 可変なローカル変数の宣言
+    for arg in std::env::args().skip(1) {
+        numbers.push(u64::from_str(&arg).expect("error parsing argument"));
+    }
+
+    if numbers.len() == 0 {
+        writeln!(std::io::stderr(), "Usage: gcd NUMBER...").unwrap();
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+    println!("The greatest common divisor of {:?} id {}", numbers, d);
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
