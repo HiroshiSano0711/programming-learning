578b45f9cabe41b466df9e3100b59ce9830ddd03
SanoHiroshi <hroshi.sano0711@gmail.com>
2019-11-05 20:44:35 +0900

    トークン化まで

--- my-compiler/lexer.c
+++ my-compiler/lexer.c
@@ -1,51 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
+#include <ctype.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-static char *read_file_contents() {
-  FILE *fp;
-  long file_size;
-  int file_size_max = 10 * 1024 * 1024;
-  struct stat stbuf;
-  char *buffer;
-  int fd;
+char *read_file(char *path) {
+  // ファイルを開く
+  FILE *fp = fopen(path, "r");
+  if (!fp)
+    printf("cannot open %s", path);
 
-  fd = open("main.txt", O_RDONLY);
-  if (fd == -1)
-      printf("cant open file : %s.\n", "main.txt");
+  // ファイルの長さを調べる
+  if (fseek(fp, 0, SEEK_END) == -1)
+    printf("%s: fseek", path);
+  size_t size = ftell(fp);
+  if (fseek(fp, 0, SEEK_SET) == -1)
+    printf("%s: fseek", path);
 
-  fp = fdopen(fd, "rb");
-  if (fp == NULL)
-    printf("cant open file : %s.\n", "main.txt");
+  // ファイル内容を読み込む
+  char *buf = calloc(1, size + 2);
+  fread(buf, size, 1, fp);
 
-  if (fstat(fd, &stbuf) == -1)
-    printf("cant get file state : %s.\n", "main.txt");
-
-  file_size = stbuf.st_size;
-
-  if(file_size > file_size_max)
-    printf("file too large");
-
-  buffer = malloc(file_size);
-  fread(buffer, 1, file_size, fp);
-
-  if (fclose(fp) != 0)
-    printf("cant close file : %s.\n", "main.txt");
-
-  return buffer;
+  // ファイルが必ず"\n\0"で終わっているようにする
+  if (size == 0 || buf[size - 1] != '\n')
+    buf[size++] = '\n';
+  buf[size] = '\0';
+  fclose(fp);
+  return buf;
 }
 
+typedef enum {
+  NUMBER,
+  ADD
+} TokenKind;
+
+typedef struct Token Token;
+struct Token {
+  TokenKind kind;
+  Token     *next;
+  int       value;
+  char      str[100];
+};
+
+Token *token;
+
+Token *new_token(TokenKind kind, Token *list) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  list->next = tok;
+  return tok;
+}
+void tokenize(char *p);
+
 int main(int argc, char const *argv[]) {
-  char *file_contents = read_file_contents();
-
-  while(*file_contents){
-    printf("%s", file_contents);
-    file_contents++;
-  }
-
+  char *file_contents = read_file("main.txt");
+  tokenize(file_contents);
+  program();
   return 0;
+}
+
+/*
+
+文法規則
+stmt     = expr ";"
+expr     = add
+add      = num ("+" num)*
+
+*/
+void statement(){
+  // 式が１つ以上
+  Node *node = expr();
+  // 次の文字がセミコロン
+
+}
+
+void tokenize(char *p) {
+  Token head;
+  head.next = NULL;
+  Token *list = &head;
+
+  while(*p){
+    if(isspace(*p)){
+      p++;
+      continue;
+    }
+    if (strchr("+", *p)) {
+      list = new_token(ADD, list);
+      strncpy(list->str, p, 1);
+      p++;
+      continue;
+    }
+    if (isdigit(*p)){
+      list = new_token(NUMBER, list);
+      list->value = strtol(p, &p, 10);
+      continue;
+    }
+    printf("文法エラー： この文字は使えません。 %s", p);
+    exit(1);
+  }
+  token = head.next;
 }
