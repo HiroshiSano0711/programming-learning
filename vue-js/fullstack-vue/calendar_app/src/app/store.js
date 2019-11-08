efb567ebb32b500c9e9672267432756572e01359
SanoHiroshi <hroshi.sano0711@gmail.com>
2019-11-09 03:12:34 +0900

    カレンダーの予定の更新

--- vue-js/fullstack-vue/calendar_app/src/app/store.js
+++ vue-js/fullstack-vue/calendar_app/src/app/store.js
@@ -16,5 +16,31 @@ export const store = {
   submitEvent(eventDetails) {
     const activeDay = this.getActiveDay();
     activeDay.events.push({ "details": eventDetails, "edit": false });
+  },
+  getEventOjb(dayId, eventDetails) {
+    const dayObj = this.state.seedData.find(
+      day => day.id === dayId
+    );
+    console.log(dayObj.events.find(event => event.details === eventDetails));
+    return dayObj.events.find(
+      event => event.details === eventDetails
+    );
+  },
+  editEvent(dayId, eventDetails) {
+    this.resetEditOfAllEvents();
+    const eventObj = this.getEventOjb(dayId, eventDetails);
+    eventObj.edit = true;
+  },
+  updateEvent(dayId, originalEventDetials, newEventDetails) {
+    const eventObj = this.getEventOjb(dayId, originalEventDetials);
+    eventObj.details = newEventDetails;
+    eventObj.edit = false;
+  },
+  resetEditOfAllEvents() {
+    this.state.seedData.map((dayObj) => {
+      dayObj.events.map((event) => {
+        event.edit = false;
+      });
+    });
   }
 }
