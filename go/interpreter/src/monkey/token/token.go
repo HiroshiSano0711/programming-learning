03eb05cd0e5ee6cb84371fc2a6105fd1e0cb0285
SanoHiroshi <hroshi.sano0711@gmail.com>
2019-11-10 01:13:09 +0900

    Go言語で字句解析の基盤を作るまで

---
+++ go/interpreter/src/monkey/token/token.go
@@ -0,0 +1,60 @@
+package token
+
+// TokenType indicates syntax type
+type TokenType string
+
+// const define syntax value
+const (
+	ILLEGAL = "ILLEGAL" // 未知のトークン
+	EOF     = "EOF"     // ファイルの終端を示す
+
+	IDENT     = "IDENT" // add, foobar, x, y...
+	INT       = "INT"
+	ASSIGN    = "="
+	PLUS      = "+"
+	MINUS     = "-"
+	BANG      = "!"
+	ASTERISK  = "*"
+	SLASH     = "/"
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	LT        = "<"
+	GT        = ">"
+	EQ        = "=="
+	NOT_EQ    = "!="
+	FUNCTION  = "FUNCTION"
+	LET       = "LET"
+	TRUE      = "TRUE"
+	FALSE     = "FALSE"
+	IF        = "IF"
+	ELSE      = "ELSE"
+	RETURN    = "RETURN"
+)
+
+// Token is syntax token
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+// LookupIdent find or create identify
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
