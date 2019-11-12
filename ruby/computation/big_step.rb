6e7247470c6fd38b737098db069c80760f6add2f
SanoHiroshi <hroshi.sano0711@gmail.com>
2019-11-12 23:24:40 +0900

    ビッグステップによる文の実行

--- ruby/computation/big_step.rb
+++ ruby/computation/big_step.rb
@@ -82,9 +82,79 @@ class LessThan < Struct.new(:left, :right)
   end
 end
 
-Number.new(23).evaluate({})
-Variable.new(:x).evaluate({ x: Number.new(23) })
-LessThan.new(
-  Add.new(Variable.new(:x), Number.new(2)),
-  Variable.new(:y)
-).evaluate({ x: Number.new(2), y: Number.new(5) })
+class Assign < Struct.new(:name, :expression)
+  def to_s
+    "#{name} = #{expression}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def evaluate(environment)
+    environment.merge({ name => expression.evaluate(environment) })
+  end
+end
+
+class DoNothing
+  def to_s
+    'do-nothing'
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def evaluate(environment)
+    environment
+  end
+end
+
+class If < Struct.new(:condition, :consequence, :alternative)
+  def evaluate(environment)
+    case condition.evaluate(environment)
+    when Boolean.new(true)
+      consequence.evaluate(environment)
+    when Boolean.new(false)
+      alternative.evaluate(environment)
+    end
+  end
+end
+
+class Sequence < Struct.new(:first, :second)
+  def evaluate(environment)
+    second.evaluate(first.evaluate(environment))
+  end
+end
+
+class While < Struct.new(:condition, :body)
+  def evaluate(environment)
+    case condition.evaluate(environment)
+    when Boolean.new(true)
+      evaluate(body.evaluate(environment))
+    when Boolean.new(false)
+      environment
+    end
+  end
+end
+
+
+
+statement = While.new(
+    LessThan.new(Variable.new(:x), Number.new(5)),
+    Assign.new(:x, Multiply.new(Variable.new(:x), Number.new(3)))
+  )
+p statement.evaluate({ x: Number.new(1) })
+
+# statement = Sequence.new(
+#   Assign.new(:x, Add.new(Number.new(1), Number.new(1))),
+#   Assign.new(:y, Add.new(Variable.new(:x), Number.new(3))),
+# )
+# p statement.evaluate({})
+
+# Number.new(23).evaluate({})
+# Variable.new(:x).evaluate({ x: Number.new(23) })
+# LessThan.new(
+#   Add.new(Variable.new(:x), Number.new(2)),
+#   Variable.new(:y)
+# ).evaluate({ x: Number.new(2), y: Number.new(5) })
