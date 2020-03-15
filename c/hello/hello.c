4387f63543e1c4054a1d646b95a86abe5268c9c5
SanoHiroshi <hroshi.sano0711@gmail.com>
2020-03-15 17:10:24 +0900

    3/15勉強会

---
+++ c/hello/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// int main(){
+//   int array[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//   printf("array[-1] = %d\n", array[-1]);
+//   printf("array[-2] = %d\n", array[-2]);
+//   printf("array[-3] = %d\n", array[-3]);
+//   printf("array[10] = %d\n", array[10]);
+//   printf("array[11] = %d\n", array[11]);
+//   printf("0[array] = %d\n", 0[array]);
+//   return 0;
+// }
