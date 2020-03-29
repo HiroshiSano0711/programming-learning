e7993e479a940de950a6693f77db33c656e7fd35
SanoHiroshi <hroshi.sano0711@gmail.com>
2020-03-29 14:27:01 +0900

    TCP/IPの学習。ソケットを作って送信するまで

---
+++ c/network/tcp-server.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main() {
+ int sock0;
+ struct sockaddr_in addr;
+ struct sockaddr_in client;
+ socklen_t clilen;
+ int sock;
+
+ /* ソケットの作成 */
+ sock0 = socket(AF_INET, SOCK_STREAM, 0);
+
+ /* ソケットの設定 */
+ addr.sin_family = AF_INET;
+ addr.sin_port = htons(10240);
+ addr.sin_addr.s_addr = INADDR_ANY;
+ bind(sock0, (struct sockaddr *)&addr, sizeof(addr));
+
+ /* TCPクライアントからの接続要求を待てる状態にする */
+ listen(sock0, 5);
+
+ /* TCPクライアントからの接続要求を受け付ける */
+ clilen = sizeof(client);
+ sock = accept(sock0, (struct sockaddr *)&client, &clilen);
+
+ /* 5文字送信 */
+ write(sock, "HELLO", 5);
+
+ /* TCPセッションの終了 */
+ close(sock);
+
+ /* listen するsocketの終了 */
+ close(sock0);
+
+ return 0;
+}
