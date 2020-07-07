952bd29c5fedacd74f4304a76564b4e30a5597ca
SanoHiroshi <hroshi.sano0711@gmail.com>
2020-07-07 21:32:48 +0900

    15回目CS勉強会

--- c/steve/9day/P2.c
+++ c/steve/9day/P2.c
@@ -1,54 +1,80 @@
 #include <stdio.h>
 
 //// 下記の再帰処理を繰り返し処理に書き直す ////
+// 関数名をつけるなら「スペースを読み飛ばす」的な処理→skip_space
+// 機能名とロジックは異なる
+// 意味を書くというのは機能名や意図を書くこと→変数名や関数名とか
 // void f() {
-//   if (getchar() == ' '){
-//     f();
-//   }
+//     if (getchar() == ' '){
+//         // printf("*\n");
+//         f();
+//     }
 // }
 // 繰り返し処理
-// void f() {
-//   while (getchar() != ' ')
-//     ;
+// void f2() {
+//     // 先に処理を抽象化しておけば書き換えも簡単になる。違う書き方もできるようになる
+//     while (getchar() == ' ')
+//         printf("*\n");
 // }
 // int main(){
-//   f();
-//   return 0;
+//     f2();
+//     return 0;
 // }
 
 /* 
 下記の再帰処理を繰り返し処理に書き直す
 n < 100と考えて良い
+
+パターン1
+①行きがけ順
+②再帰f()
+③帰りがけ順
+
+パターン2
+①行きがけ順
+②再帰f()
+③真ん中
+④再帰f()
+⑤帰りがけ順
+
 */
 // void g(n) {
 //     int i;
 //     if (n > 0){
 //         scanf("%d", &i);
 //         g(n - 1);
-//         printf("%d\n", i);
 //     }
 // }
 
 // 繰り返し処理
 // void g(n) {
-//     int i[100], j;
+//     int number[100], j;
 //     for (j = 0; j < n; j++){
-//         scanf("%d", &i[j]);
+//         scanf("%d", &number[j]);
 //     }
 //     for(j = n - 1; j >= 0; j--){
-//         printf("%d\n", i[j]);
+//         printf("%d\n", number[j]);
 //     }
 // }
 
 // int main(){
-//     g(10);
+//     g(3);
 //     return 0;
 // }
 
 //// 下記の再帰処理を繰り返し処理に書き直す ////
-// int h(n) {
-//     return n < 0 ? 0 : n == 0 ? 1 : h(n - 1) + h(n - 2);
-// }
+int h(n) {
+    return n < 0 ? 0 : n == 0 ? 1 : h(n - 1) + h(n - 2);
+}
+int h(n) {
+    if (n < 0){
+        return 0;
+    }else if(n == 0){
+        return 1;
+    }else{
+        return h(n - 1) + h(n - 2);
+    }
+}
 
 // 繰り返し処理
 int h(n) {
@@ -56,7 +82,12 @@ int h(n) {
     first = 0;
     second = 1;
 
-    for (int i = 0; i < n; i++){
+    if (n < 0){
+        return 0;
+    }else if(n == 0){
+        return 1;
+    }
+    for (int i = 1; i <= n; i++){
         next_second = first + second;
         first = second;
         second = next_second;
@@ -65,7 +96,7 @@ int h(n) {
 }
 
 int main(){
-    int a = h(10);
+    int a = h(5);
     printf("%d\n", a);
     return 0;
 }
