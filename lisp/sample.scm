b70c0a5498b3ab7a8e3239854698ba845f4ecd4c
HiroshiSano0711 <hiroshi.sano0711@gmail.com>
2021-05-02 17:21:33 +0900

    SICP問題1.9 1.10まで

--- lisp/sample.scm
+++ lisp/sample.scm
@@ -233,3 +233,65 @@ b
   (sqrt-iter 0 x x))
 
 (sqrt 8.0)
+
+
+; 問題1.9
+;(+ 4 5)
+
+;(define (+ a b)
+;(if (= a 0) b (inc (+ (dec a) b))))
+
+;(+ 4 5)
+;(inc (+ 3 5))
+;(inc (inc (+ 2 5)))
+;(inc (inc (inc (+ 1 5))))
+;(inc (inc (inc (inc (+ 0 5))))
+;(inc (inc (inc (inc 5))))
+;(inc (inc (inc 6)))
+;(inc (inc 7))
+;(inc 8)
+;9
+
+;(define (+ a b)
+;  (if (= a 0) b (+ (dec a) (inc b))))
+
+;(+ 4 5)
+;(+ 3 6)
+;(+ 2 7)
+;(+ 1 8)
+;(+ 0 9)
+;9
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1) (6A x (- y 1))))))
+
+(A 1 10)
+(A 2 4)
+(A 3 3)
+
+(define (f n) (A 0 n)) ; 2n
+(define (g n) (A 1 n)) ; 2^n
+(define (h n) (A 2 n)) ; 2の累乗の累乗
+
+    2
+   2
+  2
+ 2
+2
+
+(define (k n) (* 5 n n))
+
+(A 2 3)
+(A 1 (A 2 2))
+(A 1 (A 1 (A 2 1)))
+(A 1 (A 1 2))
+(A 1 (A 0 (A 1 1)))
+(A 1 (A 0 2))
+(A 1 4)
+(g 4)
+
+(h 4)
+(A 0 (A 1 3))
