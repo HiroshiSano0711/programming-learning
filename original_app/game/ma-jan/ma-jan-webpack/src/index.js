4aa039b5cce29eba0228fe1a986834d4d6e707cf
HiroshiSano0711 <hiroshi.sano0711@gmail.com>
2023-08-06 14:21:49 +0900

    牌画の画像を差し替え。読みやすいようにフォントやレイアウトを簡単に調整

--- original_app/game/ma-jan/ma-jan-webpack/src/index.js
+++ original_app/game/ma-jan/ma-jan-webpack/src/index.js
@@ -1,3 +1,5 @@
+import "./style.scss";
+
 // import { createRequire } from 'module';
 // const require = createRequire(import.meta.url);
 // const Majiang = require('@kobalab/majiang-core');
@@ -199,82 +201,51 @@ const paiYama = [
 ];
 
 let pai_type = [
-	// {"No":0,"paiName":"赤五万","cssSprite":"man0"},
-	// {"No":1,"paiName":"一万","cssSprite":"man1"},
-	// {"No":2,"paiName":"二万","cssSprite":"man2"},
-	// {"No":3,"paiName":"三万","cssSprite":"man3"},
-	// {"No":4,"paiName":"四万","cssSprite":"man4"},
-	// {"No":5,"paiName":"五万","cssSprite":"man5"},
-	// {"No":6,"paiName":"六万","cssSprite":"man6"},
-	// {"No":7,"paiName":"七万","cssSprite":"man7"},
-	// {"No":8,"paiName":"八万","cssSprite":"man8"},
-	// {"No":9,"paiName":"九万","cssSprite":"man9"},
+	{"No":0,"paiName":"赤五万","cssSprite":"man-0"},
+	{"No":1,"paiName":"一万","cssSprite":"man-1"},
+	{"No":2,"paiName":"二万","cssSprite":"man-2"},
+	{"No":3,"paiName":"三万","cssSprite":"man-3"},
+	{"No":4,"paiName":"四万","cssSprite":"man-4"},
+	{"No":5,"paiName":"五万","cssSprite":"man-5"},
+	{"No":6,"paiName":"六万","cssSprite":"man-6"},
+	{"No":7,"paiName":"七万","cssSprite":"man-7"},
+	{"No":8,"paiName":"八万","cssSprite":"man-8"},
+	{"No":9,"paiName":"九万","cssSprite":"man-9"},
 
-	{"No":10,"paiName":"赤五筒","cssSprite":"pin0"},
-	{"No":11,"paiName":"一筒","cssSprite":"pin1"},
-	{"No":12,"paiName":"二筒","cssSprite":"pin2"},
-	{"No":13,"paiName":"三筒","cssSprite":"pin3"},
-	{"No":14,"paiName":"四筒","cssSprite":"pin4"},
-	{"No":15,"paiName":"五筒","cssSprite":"pin5"},
-	{"No":16,"paiName":"六筒","cssSprite":"pin6"},
-	{"No":17,"paiName":"七筒","cssSprite":"pin7"},
-	{"No":18,"paiName":"八筒","cssSprite":"pin8"},
-	{"No":19,"paiName":"九筒","cssSprite":"pin9"},
+	{"No":10,"paiName":"赤五筒","cssSprite":"pin-0"},
+	{"No":11,"paiName":"一筒","cssSprite":"pin-1"},
+	{"No":12,"paiName":"二筒","cssSprite":"pin-2"},
+	{"No":13,"paiName":"三筒","cssSprite":"pin-3"},
+	{"No":14,"paiName":"四筒","cssSprite":"pin-4"},
+	{"No":15,"paiName":"五筒","cssSprite":"pin-5"},
+	{"No":16,"paiName":"六筒","cssSprite":"pin-6"},
+	{"No":17,"paiName":"七筒","cssSprite":"pin-7"},
+	{"No":18,"paiName":"八筒","cssSprite":"pin-8"},
+	{"No":19,"paiName":"九筒","cssSprite":"pin-9"},
 
-	// {"No":20,"paiName":"赤五索","cssSprite":"sou0"},
-	// {"No":21,"paiName":"一索","cssSprite":"sou1"},
-	// {"No":22,"paiName":"二索","cssSprite":"sou2"},
-	// {"No":23,"paiName":"三索","cssSprite":"sou3"},
-	// {"No":24,"paiName":"四索","cssSprite":"sou4"},
-	// {"No":25,"paiName":"五索","cssSprite":"sou5"},
-	// {"No":26,"paiName":"六索","cssSprite":"sou6"},
-	// {"No":27,"paiName":"七索","cssSprite":"sou7"},
-	// {"No":28,"paiName":"八索","cssSprite":"sou8"},
-	// {"No":29,"paiName":"九索","cssSprite":"sou9"},
+	{"No":20,"paiName":"赤五索","cssSprite":"sou-0"},
+	{"No":21,"paiName":"一索","cssSprite":"sou-1"},
+	{"No":22,"paiName":"二索","cssSprite":"sou-2"},
+	{"No":23,"paiName":"三索","cssSprite":"sou-3"},
+	{"No":24,"paiName":"四索","cssSprite":"sou-4"},
+	{"No":25,"paiName":"五索","cssSprite":"sou-5"},
+	{"No":26,"paiName":"六索","cssSprite":"sou-6"},
+	{"No":27,"paiName":"七索","cssSprite":"sou-7"},
+	{"No":28,"paiName":"八索","cssSprite":"sou-8"},
+	{"No":29,"paiName":"九索","cssSprite":"sou-9"},
 
-	// {"No":30,"paiName":"裏","cssSprite":"ji0"},
-	// {"No":31,"paiName":"東","cssSprite":"ji1"},
-	// {"No":32,"paiName":"南","cssSprite":"ji2"},
-	// {"No":33,"paiName":"西","cssSprite":"ji3"},
-	// {"No":34,"paiName":"北","cssSprite":"ji4"},
-	// {"No":35,"paiName":"白","cssSprite":"ji5"},
-	// {"No":36,"paiName":"發","cssSprite":"ji6"},
-	// {"No":37,"paiName":"中","cssSprite":"ji7"}
+	{"No":30,"paiName":"裏","cssSprite":"ji-0"},
+	{"No":31,"paiName":"東","cssSprite":"ji-1"},
+	{"No":32,"paiName":"南","cssSprite":"ji-2"},
+	{"No":33,"paiName":"西","cssSprite":"ji-3"},
+	{"No":34,"paiName":"北","cssSprite":"ji-4"},
+	{"No":35,"paiName":"白","cssSprite":"ji-5"},
+	{"No":36,"paiName":"發","cssSprite":"ji-6"},
+	{"No":37,"paiName":"中","cssSprite":"ji-7"}
 ];
 
 // import chinitsu_all_pattern from "./data/all.json" assert { type: "json" };
 import tenpai_all from "./data/tenpai/all.json" assert { type: "json" };
-// import tenpai_kanko_0 from "./data/tenpai/kanko-0.json" assert { type: "json" };
-// import tenpai_kanko_1 from "./data/tenpai/kanko-1.json" assert { type: "json" };
-// import tenpai_kanko_2 from "./data/tenpai/kanko-2.json" assert { type: "json" };
-// import tenpai_kanko_3 from "./data/tenpai/kanko-3.json" assert { type: "json" };
-// import shanten_1_all from "./data/shanten_1/all.json" assert { type: "json" };
-// import shanten_1_kanko_0 from "./data/shanten_1/kanko-0.json" assert { type: "json" };
-// import shanten_1_kanko_1 from "./data/shanten_1/kanko-1.json" assert { type: "json" };
-// import shanten_1_kanko_2 from "./data/shanten_1/kanko-2.json" assert { type: "json" };
-// import shanten_1_kanko_3 from "./data/shanten_1/kanko-3.json" assert { type: "json" };
-// import shanten_2_all from "./data/shanten_2/all.json" assert { type: "json" };
-// import shanten_2_kanko_0 from "./data/shanten_2/kanko-0.json" assert { type: "json" };
-// import shanten_2_kanko_1 from "./data/shanten_2/kanko-1.json" assert { type: "json" };
-// import shanten_2_kanko_2 from "./data/shanten_2/kanko-2.json" assert { type: "json" };
-// import shanten_2_kanko_3 from "./data/shanten_2/kanko-3.json" assert { type: "json" };
-
-// console.log(tenpai_all.length)
-// console.log(shanten_1_all.length)
-// console.log(shanten_2_all.length)
-
-// console.log(tenpai_kanko_0.length)
-// console.log(tenpai_kanko_1.length)
-// console.log(tenpai_kanko_2.length)
-// console.log(tenpai_kanko_3.length)
-// console.log(shanten_1_kanko_0.length)
-// console.log(shanten_1_kanko_1.length)
-// console.log(shanten_1_kanko_2.length)
-// console.log(shanten_1_kanko_3.length)
-// console.log(shanten_2_kanko_0.length)
-// console.log(shanten_2_kanko_1.length)
-// console.log(shanten_2_kanko_2.length)
-// console.log(shanten_2_kanko_3.length)
 
 window.addEventListener("DOMContentLoaded", function() {
 	const start_btn = document.getElementById("start_btn")
@@ -313,332 +284,3 @@ window.addEventListener("DOMContentLoaded", function() {
     console.log(count.length)
 	}
 });
-
-// 反転データ
-// let hanten = []
-// shanten_2_all.forEach(element =>{
-// 	let haishi = element.haishi
-// 	let hanten_arr = []
-// 	Array.prototype.forEach.call(haishi, (h) =>{
-// 		if(h == '1'){
-// 			hanten_arr.unshift('9')
-// 		}
-// 		if(h == '2'){
-// 			hanten_arr.unshift('8')
-// 		}
-// 		if(h == '3'){
-// 			hanten_arr.unshift('7')
-// 		}
-// 		if(h == '4'){
-// 			hanten_arr.unshift('6')
-// 		}
-// 		if(h == '5'){
-// 			hanten_arr.unshift('5')
-// 		}
-// 		if(h == '6'){
-// 			hanten_arr.unshift('4')
-// 		}
-// 		if(h == '7'){
-// 			hanten_arr.unshift('3')
-// 		}
-// 		if(h == '8'){
-// 			hanten_arr.unshift('2')
-// 		}
-// 		if(h == '9'){
-// 			hanten_arr.unshift('1')
-// 		}
-// 	})
-// 	let hanten_haishi = hanten_arr.join('')
-
-// 	hanten.push(hanten_haishi)
-// 	console.log(hanten)
-// });
-// let moto_haishi = shanten_2_all.map((element) => {
-// 	 return element.haishi
-// })
-
-// hanten.forEach(element =>{
-// 	let indexSat = moto_haishi.indexOf(element);
-// 	if(indexSat == -1){
-// 		console.log(element);
-// 	}
-// })
-
-// const all_chinitsu_pattern = []
-
-// let all_data_with_pai_count = []
-
-// shanten_1_kanko_2.forEach(element =>{
-// 	let count_1 = (element.haishi.match(/1/g) || []).length;
-// 	let count_2 = (element.haishi.match(/2/g) || []).length;
-// 	let count_3 = (element.haishi.match(/3/g) || []).length;
-// 	let count_4 = (element.haishi.match(/4/g) || []).length;
-// 	let count_5 = (element.haishi.match(/5/g) || []).length;
-// 	let count_6 = (element.haishi.match(/6/g) || []).length;
-// 	let count_7 = (element.haishi.match(/7/g) || []).length;
-// 	let count_8 = (element.haishi.match(/8/g) || []).length;
-// 	let count_9 = (element.haishi.match(/9/g) || []).length;
-
-// 	let for_cpp_data = `
-// 	{
-//     ${count_1},${count_2},${count_3},${count_4},${count_5},${count_6},${count_7},${count_8},${count_9},
-//     0,0,0,0,0,0,0,0,0,
-//     0,0,0,0,0,0,0,0,0,
-//     0,0,0,0,0,0,0
-//   }
-// 	`;
-// 	all_data_with_pai_count.push(for_cpp_data)
-// })
-// console.log(`${all_data_with_pai_count}`)
-
-// const group = tenpai_all.reduce((accumulator, current) => {
-// 	let agarihai_count = 0
-// 	current.machi.forEach(element => {
-// 		agarihai_count += 4
-// 		let used_count = (current.haishi.match(new RegExp(element, "g")) || []).length;
-// 		agarihai_count -= used_count
-// 	});
-//   const element = accumulator.find((p) => p.agarihai_count === agarihai_count);
-//   if (element) {
-//     element.count ++;
-//   } else {
-//     accumulator.push({
-//       count: 1,
-//       agarihai_count: agarihai_count
-//     });
-//   }
-//   return accumulator;
-// }, []);
-
-// console.log(group)
-
-// const new_all_data = chinitsu_all_pattern.map(element => {
-// 	let count_1 = (element.haishi.match(/1/g) || []).length;
-// 	let count_2 = (element.haishi.match(/2/g) || []).length;
-// 	let count_3 = (element.haishi.match(/3/g) || []).length;
-// 	let count_4 = (element.haishi.match(/4/g) || []).length;
-// 	let count_5 = (element.haishi.match(/5/g) || []).length;
-// 	let count_6 = (element.haishi.match(/6/g) || []).length;
-// 	let count_7 = (element.haishi.match(/7/g) || []).length;
-// 	let count_8 = (element.haishi.match(/8/g) || []).length;
-// 	let count_9 = (element.haishi.match(/9/g) || []).length;
-// 	element.pai_kind = [count_1, count_2, count_3, count_4, count_5, count_6, count_7, count_8, count_9]	
-// 	if(element.shanten == 0){
-// 		let agarihai_count = 0
-// 		element.machi.forEach(e => {
-// 			agarihai_count += 4
-// 			let used_count = (element.haishi.match(new RegExp(e, "g")) || []).length;
-// 			agarihai_count -= used_count
-// 		});
-// 		element.agarihai_count = agarihai_count
-// 	} else {
-// 		element.agarihai_count = 0
-// 	}
-// 	return element;
-// })
-// console.log(new_all_data)
-
-// const group = tenpai_all.reduce((accumulator, current) => {
-// 	let agarihai_count = 0
-// 	current.machi.forEach(element => {
-// 		agarihai_count += 4
-// 		let used_count = (current.haishi.match(new RegExp(element, "g")) || []).length;
-// 		agarihai_count -= used_count
-// 	});
-//   const element = accumulator.find((p) => p.agarihai_count === agarihai_count);
-//   if (element) {
-//     element.count ++;
-//   } else {
-//     accumulator.push({
-//       count: 1,
-//       agarihai_count: agarihai_count
-//     });
-//   }
-//   return accumulator;
-// }, []);
-
-// console.log(group)
-// let machi_1_to_8_pattern = []
-// let machi_2_to_9_pattern = []
-// let machi_1_to_7_pattern = []
-// let machi_2_to_8_pattern = []
-// let machi_3_to_9_pattern = []
-// let machi_1_to_6_pattern = []
-// let machi_2_to_7_pattern = []
-// let machi_3_to_8_pattern = []
-// let machi_4_to_9_pattern = []
-// let machi_1_to_5_pattern = []
-// let machi_2_to_6_pattern = []
-// let machi_3_to_7_pattern = []
-// let machi_4_to_8_pattern = []
-// let machi_5_to_9_pattern = []
-
-// let machi_1_to_8 = ["1", "2", "3", "4", "5", "6", "7", "8"]
-// let machi_2_to_9 = ["2", "3", "4", "5", "6", "7", "8", "9"]
-
-// let machi_1_to_7 = ["1", "2", "3", "4", "5", "6", "7"]
-// let machi_2_to_8 = ["2", "3", "4", "5", "6", "7", "8"]
-// let machi_3_to_9 = ["3", "4", "5", "6", "7", "8", "9"]
-// let machi_1_to_6 = ["1", "2", "3", "4", "5", "6"]
-// let machi_2_to_7 = ["2", "3", "4", "5", "6", "7"]
-// let machi_3_to_8 = ["3", "4", "5", "6", "7", "8"]
-// let machi_4_to_9 = ["4", "5", "6", "7", "8", "9"]
-// let machi_1_to_5 = ["1", "2", "3", "4", "5"]
-// let machi_2_to_6 = ["2", "3", "4", "5", "6"]
-// let machi_3_to_7 = ["3", "4", "5", "6", "7"]
-// let machi_4_to_8 = ["4", "5", "6", "7", "8"]
-// let machi_5_to_9 = ["5", "6", "7", "8", "9"]
-
-// tenpai_all.forEach(element => {	
-// 	if(element.shanten == 0){
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_1_to_8)){
-// 			machi_1_to_8_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_2_to_9)){
-// 			machi_2_to_9_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_1_to_7)){
-// 			machi_1_to_7_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_2_to_8)){
-// 			machi_2_to_8_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_3_to_9)){
-// 			machi_3_to_9_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_1_to_6)){
-// 			machi_1_to_6_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_2_to_7)){
-// 			machi_2_to_7_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_3_to_8)){
-// 			machi_3_to_8_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_4_to_9)){
-// 			machi_4_to_9_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_1_to_5)){
-// 			machi_1_to_5_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_2_to_6)){
-// 			machi_2_to_6_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_3_to_7)){
-// 			machi_3_to_7_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_4_to_8)){
-// 			machi_4_to_8_pattern.push(element.haishi)
-// 		}
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_5_to_9)){
-// 			machi_5_to_9_pattern.push(element.haishi)
-// 		}
-// 	}
-// })
-
-// console.log('machi_1_to_8_pattern')
-// console.log(machi_1_to_8_pattern.length)
-// console.log('machi_2_to_9_pattern')
-// console.log(machi_2_to_9_pattern.length)
-
-// console.log('machi_1_to_7_pattern')
-// console.log(machi_1_to_7_pattern.length)
-// console.log('machi_2_to_8_pattern')
-// console.log(machi_2_to_8_pattern.length)
-// console.log('machi_3_to_9_pattern')
-// console.log(machi_3_to_9_pattern.length)
-// console.log('machi_1_to_6_pattern')
-// console.log(machi_1_to_6_pattern.length)
-// console.log('machi_2_to_7_pattern')
-// console.log(machi_2_to_7_pattern.length)
-// console.log('machi_3_to_8_pattern')
-// console.log(machi_3_to_8_pattern.length)
-// console.log('machi_4_to_9_pattern')
-// console.log(machi_4_to_9_pattern.length)
-// console.log('machi_1_to_5_pattern')
-// console.log(machi_1_to_5_pattern.length)
-// console.log('machi_2_to_6_pattern')
-// console.log(machi_2_to_6_pattern.length)
-// console.log('machi_3_to_7_pattern')
-// console.log(machi_3_to_7_pattern.length)
-// console.log('machi_4_to_8_pattern')
-// console.log(machi_4_to_8_pattern.length)
-// console.log('machi_5_to_9_pattern')
-// console.log(machi_5_to_9_pattern.length)
-
-// tenpai_all.forEach(element => {	
-// 	if(element.shanten == 0){
-// 		if(JSON.stringify(element.machi) == JSON.stringify(machi_1_5_9)){
-// 			machi_1_5_9_pattern.push(element.haishi)
-// 		}
-// 	}
-// })
-
-// console.log('machi_1_2_5_9_pattern')
-// console.log(machi_1_5_9_pattern)
-
-
-
-// let machi_1_count = 0
-// let machi_2_count = 0
-// let machi_3_count = 0
-// let machi_4_count = 0
-// let machi_5_count = 0
-// let machi_6_count = 0
-// let machi_7_count = 0
-// let machi_8_count = 0
-// let machi_9_count = 0
-
-
-// tenpai_all.forEach(element => {	
-// 	if(element.shanten == 0){
-// 		console.log(element.machi)
-// 		console.log(element.machi.indexOf("8") !== -1)
-// 		if(element.machi.indexOf("1") !== -1){
-// 			machi_1_count +=1
-// 		}
-// 		if(element.machi.indexOf("2") !== -1){
-// 			machi_2_count +=1
-// 		}
-// 		if(element.machi.indexOf("3") !== -1){
-// 			machi_3_count +=1
-// 		}
-// 		if(element.machi.indexOf("4") !== -1){
-// 			machi_4_count +=1
-// 		}
-// 		if(element.machi.indexOf("5") !== -1){
-// 			machi_5_count +=1
-// 		}
-// 		if(element.machi.indexOf("6") !== -1){
-// 			machi_6_count +=1
-// 		}
-// 		if(element.machi.indexOf("7") !== -1){
-// 			machi_7_count +=1
-// 		}
-// 		if(element.machi.indexOf("8") !== -1){
-// 			machi_8_count +=1
-// 		}
-// 		if(element.machi.indexOf("9") !== -1){
-// 			machi_9_count +=1
-// 		}
-// 	}
-// })
-
-// console.log('machi_1_count')
-// console.log(machi_1_count)
-// console.log('machi_2_count')
-// console.log(machi_2_count)
-// console.log('machi_3_count')
-// console.log(machi_3_count)
-// console.log('machi_4_count')
-// console.log(machi_4_count)
-// console.log('machi_5_count')
-// console.log(machi_5_count)
-// console.log('machi_6_count')
-// console.log(machi_6_count)
-// console.log('machi_7_count')
-// console.log(machi_7_count)
-// console.log('machi_8_count')
-// console.log(machi_8_count)
-// console.log('machi_9_count')
-// console.log(machi_9_count)
--- original_app/game/ma-jan/ma-jan-webpack/webpack.config.js
+++ original_app/game/ma-jan/ma-jan-webpack/webpack.config.js
@@ -1,5 +1,23 @@
 const config = {
   entry: `./src/index.js`,
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ]
+  },
+  watchOptions: {
+    ignored: /node_modules/
+  }
 };
 export default config
