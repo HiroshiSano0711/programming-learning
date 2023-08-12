30118725711f772922e8e54273aff53f6707e028
HiroshiSano0711 <hiroshi.sano0711@gmail.com>
2023-08-12 15:04:56 +0900

    ノーテン形はひっかけ問題として含めたいので待ちとカンコの数はテンパイ形からのみフィルターするように修正

--- original_app/game/ma-jan/ma-jan-webpack/src/chinitsu-data-filter.js
+++ original_app/game/ma-jan/ma-jan-webpack/src/chinitsu-data-filter.js
@@ -83,8 +83,8 @@ export class ChinitsuDataFilter {
 
 		this._filteredData = this.#allData.filter((data) => {
 			if(!params.shantenCount.includes(String(data.shanten))) return false;
-			if(params.machiCount.length !== 0 && !params.machiCount.includes(String(data.machi.length))) return false;
-			if(params.kankoCount.length !== 0 && !params.kankoCount.includes(String(data.kanko_count))) return false;
+			if(data.shanten === 0 && params.machiCount.length !== 0 && !params.machiCount.includes(String(data.machi.length))) return false;
+			if(data.shanten === 0 && params.kankoCount.length !== 0 && !params.kankoCount.includes(String(data.kanko_count))) return false;
 			return true;
 		})
 		return this._filteredData;
